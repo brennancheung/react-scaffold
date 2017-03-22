5940896e4a68f583053f9e361765ad184c72175c
Brennan Cheung <git@brennancheung.com>
2017-03-21 20:25:09 -0700

    React Router 4 examples

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,7 +1,85 @@
 import React from 'react'
+import {
+  // BrowserRouter as Router,
+  HashRouter as Router,
+  Route,
+  Link,
+  NavLink,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+
+// const ShowProps = (props) => <pre>{JSON.stringify(props, null, 4)}</pre>
+
+const Home = () => <h1>Home</h1>
+const About = () => <h1>About</h1>
+
+const contacts = [
+  {id: 1, name: 'Brennan'},
+  {id: 2, name: 'John'},
+  {id: 3, name: 'Alex'}
+]
+
+const ListContacts = () => {
+  return (
+    <div className="contacts-list">
+      <h1>Contacts</h1>
+      <ul className="contacts-list">
+        {contacts.map(contact =>
+          <li key={contact.id}><Link to={`/contacts/${contact.id}`}>{contact.name}</Link></li>)}
+      </ul>
+    </div>
+  )
+}
+
+const EditContact = ({ match }) => {
+  const id = +match.params.id
+  const contact = contacts.find(x => x.id === id)
+  return (
+    <pre>{JSON.stringify(contact, null, 4)}</pre>
+  )
+}
+
+const ActiveNavLink = (props) => (
+  <NavLink activeClassName="active" {...props}>{props.children}</NavLink>
+)
+
+const NavMenu = () => (
+  <nav>
+    <ActiveNavLink exact to="/">Home</ActiveNavLink>
+    <ActiveNavLink to="/about">About</ActiveNavLink>
+    <ActiveNavLink exact to="/contacts">Contacts</ActiveNavLink>
+  </nav>
+)
 
 const App = () => (
-  <h1>Hello World</h1>
+  <Router>
+    <div>
+      <NavMenu />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route exact path="/contacts" component={ListContacts} />
+        <Route path="/contacts/:id" component={EditContact} />
+        <Redirect to="/about" />
+      </Switch>
+    </div>
+  </Router>
 )
 
 export default App
+
+/* Example router usage / scratchpad:
+
+
+<Route exact path="/" component={Home} />
+
+<Route path="/about" component={About} />
+
+<Route path="/rendered" render={() => <h1>Rendered directly</h1>} />
+
+<Route
+  path="/about"
+  children={({match}) => match && <h1>About</h1>} />
+
+*/
--- src/app/index.js
+++ src/app/index.js
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './reducers'
 
+import './css/app.styl'
+
 import App from './components/App.jsx'
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
